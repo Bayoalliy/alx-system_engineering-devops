182ce706aeab465d3968d3c79db95d891d29689b
adebayo <adebayoalliy@gmail.com>
2024-07-23 15:01:19 -0700

    ppupoet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 exec {'echo':
-    path => 'usr/bin/env bash',
+    path => 'usr/bin:/bin',
     command => 'echo "    BatchMode yes\n    IdentityFile ~/.ssh/school" >> ~/.ssh/ssh_config',
     returns => [0, 1],
 }
