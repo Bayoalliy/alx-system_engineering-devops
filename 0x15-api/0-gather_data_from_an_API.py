369f30680f2b9122fe2a45378ab20094ec22b2ff
adebayo <adebayoalliy@gmail.com>
2024-08-18 23:34:05 -0700

    apiiii alpahab

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ Script that uses JSONPlaceholder API to get information about employee """
 
-from sys import argv
 import requests
+from sys import argv
 
 if __name__ == '__main__':
     NUMBER_OF_DONE_TASKS = 0
